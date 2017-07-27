4aa8687ce2f1ad6a51980a51d58d84b40cc8c759
Adrien Maranville <Adrien.Maranville@gmail.com>
2017-07-26 22:15:28 -0600

    Initial Upload of app framework

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+// Place Code for iOS
+
+
+//Import library to help create  component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+//Create component
+const App = () => (
+  <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+  </View>
+);
+
+//Render Component
+AppRegistry.registerComponent('albums', () => App);
